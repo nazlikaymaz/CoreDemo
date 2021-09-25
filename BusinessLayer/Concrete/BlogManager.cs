ce6e4431fcc2025b52023471057292c57ba38f1e
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2021-09-25 17:22:24 +0300

    24.09.2021

--- BusinessLayer/Concrete/BlogManager.cs
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -32,9 +32,18 @@ namespace BusinessLayer.Concrete
             throw new NotImplementedException();
         }
 
+        public List<Blog> GetBlogListWithCategory()
+        {
+            return _blogDal.GetListWithCategory();
+        }
+
         public Blog GetById(int id)
         {
             throw new NotImplementedException();
+        }
+        public List<Blog> GetBlogByID(int id )
+        {
+            return _blogDal.GetListAll(x => x.BlogID == id);
         }
 
         public List<Blog> GetList()
