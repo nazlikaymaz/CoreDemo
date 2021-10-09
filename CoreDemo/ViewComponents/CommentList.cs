3e923f8712b0b604690751c4a67448b5d5fac7fc
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2021-10-10 00:36:46 +0300

    28.09.2021

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,38 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+         {
+             new UserComment
+             {
+                 ID=1,
+                 Username="Nazlı"
+             },
+             new UserComment
+             {
+                 ID=2,
+                 Username="Nazlı"
+             },
+
+              new UserComment
+             {
+                 ID=3,
+                 Username="Nazlı"
+             },
+
+
+         };
+                return View(commentvalues);
+        }
+    }
+}
