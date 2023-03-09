60630dcffbb393e28f894626c9662fea0f0233ed
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2023-03-10 01:46:38 +0300

    admin teması eklendi

--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -43,7 +43,7 @@ namespace CoreDemo
                 {
                     x.LoginPath = "/Login/Index/";
                 });
-                
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,9 +73,14 @@ namespace CoreDemo
 
             app.UseEndpoints(endpoints =>
             {
+
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+              );
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
